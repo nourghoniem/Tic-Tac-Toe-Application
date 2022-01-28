8d1d7ee90632b8c44f13e80a63409b26acd7a6ab
Alzahraa Eid <alzahraaeid62@gmail.com>
2022-01-28 22:02:47 +0200

    adding client_server_side_version1

---
+++ zahra/Client_server_v1/ClientSide/src/clientside/ClientMain.java
@@ -0,0 +1,15 @@
+
+package clientside;
+
+import java.io.IOException;
+import javafx.application.Application;
+
+
+public class ClientMain {
+
+    
+    public static void main(String[] args) throws IOException{
+        Application.launch(TicTacToeClient.class);
+    }
+    
+}
